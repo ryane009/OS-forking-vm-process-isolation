562159707335da15e296534271c17c21ef844c18
ryan008 <ryan_eng@brown.edu>
2023-02-05 20:59:32 -0500

    Trying

--- snake/src/common.c
+++ snake/src/common.c
@@ -3,7 +3,8 @@
 #include <stdlib.h>
 
 // TODO: define global variables needed for your snake! (part 1A)
-
+int g_cell_position = 0b0001;
+enum input_key g_curr_direction = INPUT_NONE;
 // Definition of global variables for game status.
 int g_game_over;
 int g_score;
