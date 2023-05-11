5d27ca9ad98a7f355963e8cd8481837c8b9d0515
ryan008 <ryan_eng@brown.edu>
2023-05-11 17:24:39 -0400

    src completed

--- kvstore/client/simple_client.cpp
+++ kvstore/client/simple_client.cpp
@@ -140,6 +140,15 @@ bool SimpleClient::MultiPut(const std::vector<std::string>& keys,
   return false;
 }
 
+/*
+In this function, given the person in question, to respect their wishes 
+on the right to be forgotten, we redact their name whenever either
+their first or last name shows up in a post. We redact the name by 
+parsing through all the different posts made by every user, and replacing 
+matches with the user request's name with a "*". 
+
+NB: For words like matter, if the user has the name "Matt," it will redact to *er
+*/
 bool SimpleClient::GDPRDelete(const std::string& user) {
   // TODO: Write your GDPR deletion code here!
   // You can invoke operations directly on the client object, like so:
@@ -154,6 +163,9 @@ bool SimpleClient::GDPRDelete(const std::string& user) {
   std::optional<std::string> users = Get("all_users");
   std::vector<std::string> user_ids;
 
+  //using strtok to parse through the string that is returned
+  //from the get, and adding it to our working vector of all
+  //user ids
   char* temp = strdup(users.value().c_str());
   temp[strlen(temp)-1] = '\0';
   char* token1;
@@ -169,13 +181,21 @@ bool SimpleClient::GDPRDelete(const std::string& user) {
   std::optional<std::string> name = Get(user);
 
   //retrieving parts of user name that we want to filter for
+  //In the case of 5B, we should have two names in the vector
+  //that we'll later search and redact for: Matt and Bleat
+  //in addition, as checking for matches are case sensitive,
+  //we preemptively change each of the std::strings to lowercase
   char* temp1 = strdup(name.value().c_str());
   char* name_token;
   std::vector<std::string> name_parts;
+
+  //parsing to retrieve different names to search for in our post archive
   name_token = std::strtok(temp1, " "); 
   while(name_token != NULL){
     std::string str(name_token);
     std::string result;
+
+    //using a transformer function to convert each String to lower case
     std::transform(str.begin(), str.end(), std::back_inserter(result),
                    [](unsigned char c){ return std::tolower(c); });
     name_parts.push_back(result);
@@ -183,10 +203,16 @@ bool SimpleClient::GDPRDelete(const std::string& user) {
   }
   delete[] temp1;
 
+  //retireiving all the post_ids from every post that our users made
+
+  //iterating through all of our users using the vector that we populated
   for(int i = 0; i < user_ids.size(); i++){
     std::vector<std::string> post_ids;
     std::string key = user_ids[i];
+    //adding posts so that we can have the key to access a given user's post_id
     key += "_posts";
+
+    //strtok again
     std::optional<std::string> posts = Get(key);
     char* temp2 = strdup(posts.value().c_str());
     temp2[strlen(temp2)-1] = '\0';
@@ -198,45 +224,41 @@ bool SimpleClient::GDPRDelete(const std::string& user) {
     }
     delete[] temp2;
 
-    for(int i = 0; i < name_parts.size(); i++){
-      std::cout << "testing post key " + name_parts[i] << std::endl;
-    }
+    //now retrieiving every post, and editing it as we need to
+    for(int i = 0; i < post_ids.size(); i++){
+      std::optional<std::string> content = Get(post_ids[i]);
+      if (content.has_value()) {
+        //we copy contents of post into text, so we can convert everything into
+        //lowercase in text without changing the original content in a way 
+        //where everything becomes lower case (we're only redacting, not changing
+        //the text content). We need to convert the text into
+        //lower-case however so that we can compare the text with name of the user we retrieved above
+        //without uppercase/lowercase getting in the way, so our checking for the user 
+        //request's name non-casesensitive
+        std::string text;
+        std::transform(content.value().begin(), content.value().end(), std::back_inserter(text),
+                    [](unsigned char c){ return std::tolower(c); });
+        for(int n = 0; n < name_parts.size(); n++){
+          size_t index = text.find(name_parts[n]);
 
-    // for(int i = 0; i < post_ids.size(); i++){
-    //   std::optional<std::string> content = Get(post_ids[i]);
-    //   std::string text;
-    //   std::transform(content.value().begin(), content.value().end(), std::back_inserter(text),
-    //                [](unsigned char c){ return std::tolower(c); });
-    //   for(int n = 0; n < name_parts.size(); n++){
-    //     size_t index = text.find(name_parts[n]);
-    //     while(index != std::string::npos){
-    //       content.value().erase(index, name_parts[n].size());
-    //       content.value().insert(index, "*");
-    //       index = text.find(name_parts[n]);
-    //     }
-    //   }
-    //   Put(post_ids[i], content.value());
-    // }
-      for(int i = 0; i < post_ids.size(); i++){
-        std::optional<std::string> content = Get(post_ids[i]);
-        if (content.has_value()) {
-          std::string text;
-          std::transform(content.value().begin(), content.value().end(), std::back_inserter(text),
-                      [](unsigned char c){ return std::tolower(c); });
-          for(int n = 0; n < name_parts.size(); n++){
-            size_t index = text.find(name_parts[n]);
-            while(index != std::string::npos){
-              content.value().erase(index, name_parts[n].size());
-              content.value().insert(index, "*");
+          //while iterating through a 
+          while(index != std::string::npos){
 
-              text.erase(index, name_parts[n].size());
-              text.insert(index, "*");
-              index = text.find(name_parts[n], index + 1);
-            }
+            //redacting actual content on the page
+            content.value().erase(index, name_parts[n].size());
+            content.value().insert(index, "*");
+
+            //making those changes in text too, so we don't double update and the
+            //indexes of the modified text and actual content are still synced
+            text.erase(index, name_parts[n].size());
+            text.insert(index, "*");
+            index = text.find(name_parts[n], index + 1);
           }
-          Put(post_ids[i], content.value());
         }
+        //updating post with new post (could be identifical, or feature redactions)
+        Put(post_ids[i], content.value());
       }
+    }
 
   }
 
